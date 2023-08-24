ed82011c91e68f7cb07fe76cbe34d015653762c5
Kamilla <KamillaKa@users.noreply.github.com>
2023-08-24 15:27:59 +0300

    24.8 ip

---
+++ WebKehityksenPerusteet/2Module/6/6.js
@@ -0,0 +1,48 @@
+'use strict';
+
+//const movies = [];
+
+let more = true;
+/*
+while (more) {
+  const title = prompt('Syötä elokuvan nimi');
+  const rating = prompt('Syötä elokuvan arvio (1-5)');
+  const movie = {title, rating};
+
+  movies.push(movie);
+
+  more = confirm('Halutko syöttää enemmän?');
+}
+*/
+
+const movies = [
+  {title: 'Neverland', rating: '4'},
+  {title: 'Siamese', rating: '2'},
+  {title: 'Loch Ness', rating: '1'},
+  {title: 'Conductor', rating: '2'},
+];
+
+function sortMovies(a, b) {
+  const difference = b.rating - a.rating;
+  return difference;
+}
+
+movies.sort(sortMovies);
+
+console.log(movies);
+for (const movie of movies) {
+  const td1 = document.createElement('td');
+  td1.innerText = movie.title;
+
+  const td2 = document.createElement('td');
+  td2.innerText = movie.rating;
+
+  const tr = document.createElement('tr');
+  tr.append(td1);
+  tr.append(td2);
+
+  document.getElementById('target').append(tr);
+}
+
+const favorite = movies[0];
+document.getElementById('fav').innerText = favorite.title;
