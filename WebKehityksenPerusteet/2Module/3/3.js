3297e0ad5b479f889fb82b81f67e8da6c8cacdeb
Kamilla <KamillaKa@users.noreply.github.com>
2023-08-29 13:31:45 +0300

    2Mod 3 Done

---
+++ WebKehityksenPerusteet/2Module/3/3.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const numbers = [];
+let cont = true;
+while (cont) {
+  const number = prompt('Syötä luku tai kirjoita stop lopettaaksesi');
+  if (number == 'stop') {
+    alert('Lopetit ohjelman');
+    cont = false;
+  }
+  numbers.push(number);
+}
+
+document.getElementById('target').innerHTML == 'Tasaluvut';
+const even = [];
+for (const number of numbers) {
+  if (number % 2 === 0) {
+    even.push(number);
+  }
+}
+
+if (even.length > 0) {
+  document.getElementById('target').innerHTML += even.join(' , ');
+} else {
+  document.getElementById('target').innerHTML += 'Ei ole';
+}
