9e1f910e6c4dd6f074ad4ee7c67e0ac76dcea79c
Kamilla <KamillaKa@users.noreply.github.com>
2023-09-13 15:24:16 +0300

    from fixing

--- WebKehityksenPerusteet/Bundler/src/functions/greeting.ts
+++ WebKehityksenPerusteet/Bundler/src/functions/greeting.ts
@@ -1,7 +1,5 @@
-import {greeting} from
-
-const h1Element = (name: string) => {
-    return `Hello ${name}`!;
+const greeting = (name: string) => {
+    return `Hello ${name}!`;
     };
 
-    export (greeting);
+export (greeting);
