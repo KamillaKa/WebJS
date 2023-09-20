d525d47482c08c0a40707c684b6b46ea77cbd361
Kamilla <KamillaKa@users.noreply.github.com>
2023-09-20 17:06:41 +0300

    1ts

---
+++ WebKehityksenPerusteet/1Typescript/1.ts
@@ -0,0 +1,25 @@
+interface Item {
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+const cart: Item[] = [];
+
+while (true) {
+    const itemName = prompt("Enter item name:");
+
+    if (itemName === "") {
+        break;
+    }
+
+    const itemPrice = parseFloat(prompt("Enter item price:"));
+    const itemQuantity = parseInt(prompt("Enter item quantity:"));
+
+    const newItem: Item = { name: itemName, price: itemPrice, quantity: itemQuantity };
+    cart.push(newItem);
+}
+
+const totalCost = cart.map(item => item.price * item.quantity).reduce((sum, cost) => sum + cost, 0);
+
+console.log(`Total cost of the shopping cart: $${totalCost.toFixed(2)}`);
