cc81e6d4ad35238eecd3d9bff59ea4bb7d5f7ba6
Kamilla <KamillaKa@users.noreply.github.com>
2023-09-29 22:30:00 +0300

    UIUX done

--- WebKehityksenPerusteet/UIUX/Seefood/ownDialog.js
+++ WebKehityksenPerusteet/UIUX/Seefood/ownDialog.js
@@ -1,59 +1,65 @@
-const dialog = document.querySelector('.seafood-dialog');
-const openDialogBtn = document.querySelector('.seacre');
-const closeDialogBtn = document.querySelector('.closeSeacre');
+// const dialogActions = [
+//   { dialog: 'dialogCrab', show: 'showCrab', close: 'closeCrab' },
+//   { dialog: 'dialogClam', show: 'showClam', close: 'closeClam' },
+//   { dialog: 'dialogHummus', show: 'showHummus', close: 'closeHummus' },
+//   { dialog: 'dialogCod', show: 'showCod', close: 'closeCod' }
+// ];
 
-const elements = dialog.querySelectorAll(
-  'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])'
-);
-const firstElement = elements[0];
-const lastElement = elements[elements.length - 1];
+// dialogActions.forEach(action => {
+//   const dialog = document.getElementById(action.dialog);
+//   document.getElementById(action.show).onclick = () => dialog.show();
+//   document.getElementById(action.close).onclick = () => dialog.close();
+// });
 
-const trapFocus = e => {
-  if (e.key === 'Tab') {
-    const tabForwards = !e.shiftKey && document.activeElement === lastElement;
-    const tabBackwards = e.shiftKey && document.activeElement === firstElement;
-    if (tabForwards) {
-      // only TAB is pressed, not SHIFT simultaneously
-      // Prevent default behavior of keydown on TAB (i.e. focus next element)
+
+const createDialog = (dialogId, openBtnId, closeBtnId) => {
+  const dialog = document.getElementById(dialogId);
+  const openDialogBtn = document.getElementById(openBtnId);
+  const closeDialogBtn = document.getElementById(closeBtnId);
+
+  const elements = dialog.querySelectorAll(
+    'a, button, input, textarea, select, details, [tabindex]:not([tabindex="-1"])'
+  );
+  const firstElement = elements[0];
+  const lastElement = elements[elements.length - 1];
+
+  const preventClick = e => {
+    if (e.target !== closeDialogBtn) {
+      e.stopPropagation();
       e.preventDefault();
-      firstElement.focus();
-    } else if (tabBackwards) {
-      // TAB and SHIFT are pressed simultaneously
-      e.preventDefault();
-      lastElement.focus();
     }
-  }
+  };
+
+  const trapFocus = e => {
+    if (e.key === 'Tab') {
+      const tabForwards = !e.shiftKey && document.activeElement === lastElement;
+      const tabBackwards = e.shiftKey && document.activeElement === firstElement;
+      if (tabForwards || tabBackwards) {
+        e.preventDefault();
+        (tabForwards ? firstElement : lastElement).focus();
+      }
+    }
+  };
+
+  const openDialog = () => {
+    dialog.showModal();
+    dialog.addEventListener('keydown', trapFocus);
+    dialog.addEventListener('click', preventClick);
+  };
+
+  const closeDialog = e => {
+    e.preventDefault();
+    dialog.close();
+    dialog.removeEventListener('keydown', trapFocus);
+    dialog.removeEventListener('click', preventClick);
+    openDialogBtn.focus();
+  };
+
+  openDialogBtn.addEventListener('click', openDialog);
+  closeDialogBtn.addEventListener('click', closeDialog);
 };
 
-const openDialog = () => {
-  dialog.showModal();
-  dialog.addEventListener('keydown', trapFocus);
-};
-
-const closeDialog = e => {
-  e.preventDefault();
-  dialog.close();
-  dialog.removeEventListener('keydown', trapFocus);
-  openDialogBtn.focus();
-};
-
-openDialogBtn.addEventListener('click', openDialog);
-closeDialogBtn.addEventListener('click', closeDialog);
-
-if (typeof dialog.showModal !== 'function') {
-  /**
-   * How to add polyfill outside CodePen conditionally
-   * let polyfill = document.createElement("script");
-   * polyfill.type = "text/javascript";
-   * polyfill.src = "/dist/dialog-polyfill.js";
-   * document.body.append(polyfill);
-
-   * const polyfillStyles = document.createElement("link");
-   * polyfillStyles.rel = "stylesheet";
-   * polyfillStyles.href = "dialog-polyfill.css";
-   * document.head.append(polyfillStyles);
-   **/
-
-  // Register polyfill on dialog element once the script has loaded
-  dialogPolyfill.registerDialog(dialog);
-}
+createDialog('dialogCrab', 'showCrab', 'closeCrab');
+createDialog('dialogClam', 'showClam', 'closeClam');
+createDialog('dialogHummus', 'showHummus', 'closeHummus');
+createDialog('dialogCod', 'showCod', 'closeCod');
