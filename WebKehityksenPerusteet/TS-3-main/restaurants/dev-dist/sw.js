8f3f04d9b112044510f12dde372e0ef6d6287c7b
Kamilla <KamillaKa@users.noreply.github.com>
2023-10-16 19:57:02 +0300

    responsive done

--- WebKehityksenPerusteet/TS-3-main/restaurants/dev-dist/sw.js
+++ WebKehityksenPerusteet/TS-3-main/restaurants/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0k9clr4mi08"
+    "revision": "0.kret7hf54oo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
