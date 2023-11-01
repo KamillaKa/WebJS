140174ae6d3dc3a33d12d194faf243c1a4bc25f9
Kamilla <KamillaKa@users.noreply.github.com>
2023-11-01 09:10:47 +0200

    npm run build maybe fixed?

--- WebKehityksenPerusteet/TS-3-main/restaurants/dev-dist/sw.js
+++ WebKehityksenPerusteet/TS-3-main/restaurants/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kret7hf54oo"
+    "revision": "0.jcs1akruns"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
